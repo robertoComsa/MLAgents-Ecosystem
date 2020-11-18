60e00f00b976230872fcd4952c73e43bfa75b995
robertoComsa <61553028+robertoComsa@users.noreply.github.com>
2020-11-18 21:16:40 +0200

    Bug fix
    - Fixed agents not connecting with brain using Academy.Instance.Dispose() before loading Main Scene.

--- _TrainingEnvs/TrainingEnvs/Assets/_Scripts/App/Main Menu/MainMenu.cs
+++ _TrainingEnvs/TrainingEnvs/Assets/_Scripts/App/Main Menu/MainMenu.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using MLAgents;
 
 public class MainMenu : MonoBehaviour
 {
     public void LoadPrepareSimulation()
     {
+        Academy.Instance.Dispose();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
