f2b749957928b75733f27ae641df0e650c60996f
robertoComsa <61553028+robertoComsa@users.noreply.github.com>
2020-11-20 15:13:02 +0200

    Added GUI for PlacementController
    -> Simple version of grid with suggestions for button (will be updated in future)

--- _TrainingEnvs/TrainingEnvs/Assets/_Scripts/App/Main Scene/GameManager.cs
+++ _TrainingEnvs/TrainingEnvs/Assets/_Scripts/App/Main Scene/GameManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    // ---- VARIABILE VIZIBILE IN EDITOR ---- //
+
+    [Tooltip("GameObjectul canvas")][SerializeField] Canvas canvas = null;
+
+    // ------ ACCESORI ------- //
+
     public bool CanAgentsRequestDecisions { get; set; } = false;
 
+    // -------- METODE ------- //
+
+    // Apelata in fiecare frame
     private void Update()
     {
         // Activate agents
 
         if (Input.GetKeyDown(KeyCode.M))
+        {
             CanAgentsRequestDecisions = true;
+            canvas.gameObject.SetActive(false);
+        }
     }
 }
